0baee68ec1f162ef04051186699fc1c0624afc83
Dhanya Srivastava <srivastavadhanya566@gmail.com>
2026-07-14 10:58:31 +0530

    Updated Analytics file to generate Bar Graph

--- Analytics.py
+++ Analytics.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from collections import Counter
+import matplotlib.pyplot as plt
 
 try:
     
@@ -7,16 +8,14 @@ try:
     
     
     all_keywords = []
-    
-    
     for keywords_str in df['KeywordsString'].dropna():
         words = keywords_str.split()
         all_keywords.extend(words)
         
-    
+
     keyword_counts = Counter(all_keywords)
     
-
+    
     metrics_df = pd.DataFrame(keyword_counts.items(), columns=['Keyword', 'Frequency'])
     
 
@@ -25,8 +24,15 @@ try:
     
     print("\n Your Top Studied Topics & Keywords:")
     print(metrics_df.head(10))
-    print()
+
+    
+    metrics_df.head(10).plot(x='Keyword', y='Frequency', kind='bar', color='purple')
+    plt.title('Top Studied Topics & Keywords')
+    plt.xlabel('Keywords')
+    plt.ylabel('Frequency')
+    plt.xticks(rotation=45) 
+    plt.tight_layout()      
+    plt.show()
 
 except FileNotFoundError:
-    
     print("\n 'study_metrics.csv' not found. Run your Java program first to log data!\n")
